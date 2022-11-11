70fe4539be2f3de455908a14cad8ae84c21d0dbe
u31504 <u31504@P31504>
2022-11-12 08:40:28 +1000

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cherednichenko402_Test_PR2_MathTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
