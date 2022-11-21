c3e480e29e42b3c943fe1b3c0ea4ba20c3544f57
LizaCherednichenko <114129688+LizaCherednichenko@users.noreply.github.com>
2022-11-21 11:18:03 +1000

    Add files via upload

---
+++ Cherednichenko402_Test_PR2_Cylinder/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cherednichenko402_Test_PR2_Cylinder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Cylinder
+    {
+        public double RectangleArea(double r, double h)
+        {
+            return Math.PI * r*r * h;
+        }
+    }
+}
