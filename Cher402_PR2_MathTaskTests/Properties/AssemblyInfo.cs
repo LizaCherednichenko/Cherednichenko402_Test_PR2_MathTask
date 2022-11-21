536623ff2cf021b5c7868f5fd6fe2abfdb92f6d7
LizaCherednichenko <114129688+LizaCherednichenko@users.noreply.github.com>
2022-11-21 11:19:00 +1000

    Add files via upload

---
+++ Cher402_PR2_MathTaskTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Cher402_PR2_MathTaskTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Cher402_PR2_MathTaskTests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c1714043-161f-41f3-b8ab-6f858a091937")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Cher402_PR2_MathTaskTests/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Cherednichenko402_Test_PR2_MathTask;
+using Cherednichenko402_Test_PR2_Cylinder;
+
+namespace Cher402_PR2_MathTaskTests
+{
+    [TestClass]
+    public class GeometryTests
+    {
+        [TestMethod]
+        public void RectangleArea_3and5_15returned()
+        {
+            //исходные данные
+            int a = 3;
+            int b = 5;
+            int expected = 15;
+
+            //Получаю значение м помощью тестируемого метода
+            Geometry g = new Geometry();
+            int actual = g.RectangleArea(a, b);
+
+            //сравнение ожиданемого результата с полученным
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void RectangleAreaCylinderReturned()
+        {
+            double r = 5;
+            double h = 20;
+            double expected = 1571;
+
+            Cylinder a = new Cylinder();
+            double actual = a.RectangleArea(r, h);
+            ;
+
+            Assert.AreEqual(expected, Math.Round(actual));
+        }
+    }
+}
